ad67484cf6fe9423de0b3c1e74d3ec30cd054913
bestsshme <35902661+bestsshme@users.noreply.github.com>
2018-02-07 22:56:40 +0000

    Update ssl.sh

--- ssl.sh
+++ ssl.sh
@@ -38,8 +38,8 @@ END
 #membuat sertifikat
 openssl genrsa -out key.pem 2048
 openssl req -new -x509 -key key.pem -out cert.pem -days 1095 \
--subj
-"/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
+
+-subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
 cat key.pem cert.pem >> /etc/stunnel/stunnel.pem
 
 #konfigurasi stunnel

51129da11e76faae196a6dd5ab3bbf419eb7f95c
bestsshme <35902661+bestsshme@users.noreply.github.com>
2018-02-09 20:50:43 +0000

    Update ssl.sh

--- ssl.sh
+++ ssl.sh
@@ -1,22 +1,11 @@
-#!/bin/bash
-#
-# Script
-# by: juckyvengeance
-# 
-
 #detail nama perusahaan
 country=SG
-state=best-ssh.me
-locality=best-ssh.me
-organization=best-ssh.me
+state=Best-ssh.me
+locality=Best-ssh.me
+organization=Best-ssh.me
 organizationalunit=SG
-commonname=best-ssh.me
+commonname=Best-ssh.me
 email=admin@best-ssh.me
-
-# go to root
-cd
-
-curl -L "https://bintray.com/user/downloadSubjectPublicKey?username=bintray" -o Release-neofetch.key && sudo apt-key add Release-neofetch.key && rm Release-neofetch.key
 
 
 # install stunnel
@@ -38,9 +27,8 @@ END
 #membuat sertifikat
 openssl genrsa -out key.pem 2048
 openssl req -new -x509 -key key.pem -out cert.pem -days 1095 \
-
--subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email" cat key.pem cert.pem >> /etc/stunnel/stunnel.pem
-
+-subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
+cat key.pem cert.pem >> /etc/stunnel/stunnel.pem
 
 #konfigurasi stunnel
 sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4

a0b96ba42080b0c9f1000352995c0f8a16043e6f
bestsshme <35902661+bestsshme@users.noreply.github.com>
2018-03-11 18:08:08 +0700

    Update badvpn.sh

--- badvpn.sh
+++ badvpn.sh
@@ -1,8 +1,12 @@
+# disable ipv6
+echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
+sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local
+
 # install badvpn
 cd
 wget -O /usr/bin/badvpn-udpgw "http://vira.cf/badvpn-udpgw"
 if [ "$OS" == "x86_64" ]; then
-  wget -O /usr/bin/screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300"
+  wget -O /usr/bin/badvpn-udpgw "http://vira.cf/badvpn-udpgw64"
 fi
 sed -i '$ i\badvpn-udpgw --listen-addr 127.0.0.1:7300 > /dev/nul &' /etc/rc.local
 chmod +x /usr/bin/badvpn-udpgw

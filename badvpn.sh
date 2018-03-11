6529f72ca24f794bb1196bbf6070171695498c6e
bestsshme <35902661+bestsshme@users.noreply.github.com>
2018-03-11 17:23:31 +0700

    Create badvpn.sh

---
+++ badvpn.sh
@@ -0,0 +1,9 @@
+# install badvpn
+cd
+wget -O /usr/bin/badvpn-udpgw "http://vira.cf/badvpn-udpgw"
+if [ "$OS" == "x86_64" ]; then
+  wget -O /usr/bin/badvpn-udpgw "http://vira.cf/badvpn-udpgw64"
+fi
+sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300' /etc/rc.local
+chmod +x /usr/bin/badvpn-udpgw
+screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300

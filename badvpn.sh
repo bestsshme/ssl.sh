2f2c7a4da6bf6778e8183b058d7fb487863b5f3a
bestsshme <35902661+bestsshme@users.noreply.github.com>
2018-03-16 21:31:26 +0700

    Update badvpn.sh

--- badvpn.sh
+++ badvpn.sh
@@ -1,9 +1,8 @@
 # install badvpn
-
 cd
-wget -O /usr/bin/badvpn-udpgw "https://github.com/bestsshme/ssl.sh/blob/master/badvpn32"
+wget -O /usr/bin/badvpn-udpgw "http://vira.cf/badvpn-udpgw"
 if [ "$OS" == "x86_64" ]; then
-  wget -O /usr/bin/badvpn-udpgw "https://github.com/bestsshme/ssl.sh/blob/master/badvpn"
+  wget -O /usr/bin/badvpn-udpgw "http://vira.cf/badvpn-udpgw64"
 fi
 sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300' /etc/rc.local
 chmod +x /usr/bin/badvpn-udpgw
